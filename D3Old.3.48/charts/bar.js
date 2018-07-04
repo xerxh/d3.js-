a48e758e4fb136b3df6a5804624e41a2b6c99e92
xerxh <xhxer512@163.com>
2018-07-04 22:32:55 +0800

    柱状图

--- D3Old.3.48/charts/bar.js
+++ D3Old.3.48/charts/bar.js
@@ -7,13 +7,15 @@ var svg = d3.select("body")
             .attr("width", width)
             .attr("height", height);
 //  画布旁边的留白 防止图形绘制到边界上
-var padding = {left : 20, right : 20, top : 20, bottom : 20};
+var padding = {left : 30, right : 20, top : 20, bottom : 20};
 // 定义图行数据
 var yData = [10, 20, 30, 40, 50, 60];
 var xData = [2017, 2018, 2019, 2020, 2021, 2022];
 // x轴的比例尺  ordinal() 序数比例尺
 var xScale = d3.scale.ordinal()
                .domain(d3.range(xData.length)) // 定义定义域
+               // .domain([d3.min(xData), d3.max(xData)])
+               // .range([0, width - padding.left - padding.right])
                .rangeRoundBands([0, width - padding.left - padding.right])
 // y轴的比例尺 linear() 线性比例尺
 var yScale = d3.scale.linear()
@@ -35,6 +37,7 @@ var rects = svg.selectAll(".MyRect")
                .enter()
                .append("rect")
                .attr("class", "MyRect")
+               .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
                .attr("x", function (d, i) {
                    // console.log(i)
                    console.log(xScale(i))
@@ -55,7 +58,7 @@ var texts = svg.selectAll(".MyText")
                .attr("class", "MyText")
                .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
                .attr("x", function (d, i) {
-                   return xScale(i) + rectPadding/2;
+                   return xScale(i)-10;
                })
                .attr("y", function (d) {
                    return yScale(d);
